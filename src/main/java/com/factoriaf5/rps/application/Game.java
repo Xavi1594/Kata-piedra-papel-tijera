2a7552ce60fb4193a337f3c70c4afe3f3974647d
Xavi1594 <xavi9415@hotmail.com>
2023-01-26 09:38:12 +0100

    pasados test game

--- src/main/java/com/factoriaf5/rps/application/Game.java
+++ src/main/java/com/factoriaf5/rps/application/Game.java
@@ -1,5 +1,27 @@
 package com.factoriaf5.rps.application;
 
+import com.factoriaf5.rps.models.Player;
+
 public class Game {
-    
+
+    Player player1;
+    Player player2;
+
+    public Game(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    public String checkWinner() {
+        return null;
+    }
+
 }
--- src/test/java/com/factoriaf5/rps/application/GameTest.java
+++ src/test/java/com/factoriaf5/rps/application/GameTest.java
@@ -1,13 +1,51 @@
 package com.factoriaf5.rps.application;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import com.factoriaf5.rps.models.Figure;
+import com.factoriaf5.rps.models.Player;
+import com.factoriaf5.rps.models.Rock;
+import com.factoriaf5.rps.models.Scissors;
+
 public class GameTest {
-    
+
+    Player player1;
+    Player player2;
+    Game game;
+
+    public GameTest() {
+        this.player1 = new Player();
+        this.player2 = new Player();
+        this.game = new Game(player1, player2);
+    }
+
     @Test
-    public void justASimpleTest() {
-        assertTrue(true);
+    public void test_game_init_with_two_players() {
+        assertTrue(game.getPlayer1() instanceof Player);
+        assertTrue(game.getPlayer2() instanceof Player);
+    }
+
+    @Test
+    public void test_game_player1_choose_rock_player2_choose_scissors() {
+        Figure rock = game.player1.choose("Rock");
+        Figure scissors = game.player2.choose("Scissors");
+        assertTrue(rock instanceof Rock);
+        assertTrue(scissors instanceof Scissors);
+        assertEquals("Rock", rock.getType());
+        assertEquals("Scissors", scissors.getType());
+    }
+
+    @Test
+    public void test_Rock_beats_Scissors() {
+        game.player1.choose("Rock");
+        game.player2.choose("Scissors");
+
+        String result = game.checkWinner();
+
+        assertEquals("Rock beats Scissors", result);
+
     }
 }
